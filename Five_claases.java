297e81c472ac00e47acf4266d2c30ce6ed19e657
frontendInternGlynac <frontend@glynac.ai>
2026-05-19 20:34:30 +0530

    five classes homework

--- Five_claases.java
+++ Five_claases.java
@@ -1,1 +1,68 @@
-cl
+public class Bus {
+
+int id;
+string name;
+
+void sleeping(){
+    
+}
+void driving(){
+    
+}
+
+}
+
+public class Company {
+
+int employeeId;
+int salary;
+
+void working(){
+
+}
+void speaking(){
+
+}
+
+}
+
+class Examcenter {
+
+int seatNo;
+string subject;
+
+void writing(){
+
+}
+void reading(){
+
+}
+
+}
+
+public class Mall {
+
+int prductId;
+int price;
+
+void shopping(){
+
+}
+void eating(){
+
+}
+
+}
+
+public class Shop {
+
+int productPrice;
+string productName;
+
+void selling(){
+
+}
+void buying(){
+
+}
+}
